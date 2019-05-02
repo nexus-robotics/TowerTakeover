7461c556dd5c747c973a0d6cf93604eec4dbfe6d
Carson <carsonbrown47@gmail.com>
2019-05-02 16:13:03 -0400

    finished driveTo function and added drive encoders

--- PID_TEST/src/Variables.cpp
+++ PID_TEST/src/Variables.cpp
@@ -1,3 +1,6 @@
+#Define leftEnc = 1;
+#Define rightEnc = 2;
+
 #Define kP = 0; //tuning variable to scale motor powers
 #Define kI = 0;//tunning variable to eliminate small amounts of error
 #Define kD = 0;//tuning variable to account for issues with scaling the derivative
--- PID_TEST/src/autonomous.cpp
+++ PID_TEST/src/autonomous.cpp
@@ -1,29 +1,34 @@
 #include "main.h"
 #include "Variables.cpp"
 
-void myPID(int target, int sensorValue)
+void driveTo(int target)
 {
-  while (1)
+  while (power > 20)
   {
-    error = target – sensorValue;//calculates the remaining units between target and current values
+    error = target – ((adi_analog_read(leftEnc) + adi_analog_read(rightEnc)) / 2);
     integral = integral + error;
     if (error = 0 || error > target)
     {
       integral = 0;
-    }
+    }//end of if
     if (error > 1500)
     {
       integral = 0;
-    }
+    }//end of if
     derivative = error – prevError;
     prevError = error;
     power = (error*kP) + (integral*kI) + (derivative*kD);
     pros::delay(15);
-  }
-}
+  }//end of while
+  power = 0;
+}//end of driveTo function
 
 
 void autonomous()
 {
+  adi_analog_calibrate(leftEnc);// calibrates the left encoder
+  adi_analog_calibrate(rightEnc);// calibrates the right encoder
+
+  driveTo(5000);
 
 }
