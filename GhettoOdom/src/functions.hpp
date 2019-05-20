a37dcd05a26e19e9841475ae8e033479f3985e48
Farees|200X|LeadProgrammer <44035032+Fareesl200XlLeasProgrammer@users.noreply.github.com>
2019-05-20 15:09:00 -0400

    updated header file

--- GhettoOdom/src/functions.hpp
+++ GhettoOdom/src/functions.hpp
@@ -1,3 +1,8 @@
 #include "main.h"
 
 void drive(void*);
+void printXY(void*);
+void goodTurn(double target);
+void pidDrive(double target);
+void odomTurn_rad(double target);
+void driveToPoint(double x, double y);
